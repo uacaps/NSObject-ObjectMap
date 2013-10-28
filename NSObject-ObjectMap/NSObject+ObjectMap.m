369ec509d910637e1d7d75189036eceb4a39c662
Matthew York <myork@cs.ua.edu>
2013-10-28 13:31:45 -0500

    - Added support in xml parsing for primitive number arrays

--- NSObject-ObjectMap/NSObject+ObjectMap.m
+++ NSObject-ObjectMap/NSObject+ObjectMap.m
@@ -1,31 +1,32 @@
-//  Copyright (c) 2013 The Board of Trustees of The University of Alabama
-//  All rights reserved.
+//Copyright (c) 2012 The Board of Trustees of The University of Alabama
+//All rights reserved.
 //
-//  Redistribution and use in source and binary forms, with or without
-//  modification, are permitted provided that the following conditions
-//  are met:
+//Redistribution and use in source and binary forms, with or without
+//modification, are permitted provided that the following conditions
+//are met:
 //
-//  1. Redistributions of source code must retain the above copyright
-//  notice, this list of conditions and the following disclaimer.
-//  2. Redistributions in binary form must reproduce the above copyright
-//  notice, this list of conditions and the following disclaimer in the
-//  documentation and/or other materials provided with the distribution.
-//  3. Neither the name of the University nor the names of the contributors
-//  may be used to endorse or promote products derived from this software
-//  without specific prior written permission.
+//1. Redistributions of source code must retain the above copyright
+//notice, this list of conditions and the following disclaimer.
+//2. Redistributions in binary form must reproduce the above copyright
+//notice, this list of conditions and the following disclaimer in the
+//documentation and/or other materials provided with the distribution.
+//3. Neither the name of the University nor the names of the contributors
+//may be used to endorse or promote products derived from this software
+//without specific prior written permission.
 //
-//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-//  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-//  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-//  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
-//  THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
-//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-//  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-//   SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
-//  HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
-//  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
-//  OF THE POSSIBILITY OF SUCH DAMAGE.
+//THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+//"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+//LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+//FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+//THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+//INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+//(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+//STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+//ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+//OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 #import "NSObject+ObjectMap.h"
 
@@ -112,6 +113,11 @@ static const short _base64DecodingTable[256] = {
         
         // Scan and create objects until you can't no mo'
         while (![insideArrayScanner isAtEnd]) {
+            //Remove additional XML attributes from parsing
+            if ([filteredArrayObj rangeOfString:@" "].location != NSNotFound) {
+                filteredArrayObj = [filteredArrayObj substringToIndex:[filteredArrayObj rangeOfString:@" "].location];
+            }
+            
             [insideArrayScanner scanUpToString:[NSString stringWithFormat:@"<%@", filteredArrayObj] intoString:&trash];
             [insideArrayScanner scanUpToString:@">" intoString:&trash];
             [insideArrayScanner scanString:@">" intoString:&trash];
@@ -120,6 +126,9 @@ static const short _base64DecodingTable[256] = {
             // Create Object
             if ([filteredArrayObj isEqualToString:@"string"]) {
                 [objArray addObject:(NSString *)newValue];
+            }
+            else if ([filteredArrayObj isEqualToString:@"int"] || [filteredArrayObj isEqualToString:@"float"] || [filteredArrayObj isEqualToString:@"double"]){
+                [objArray addObject:[NSNumber numberWithDouble:((NSString *)newValue).doubleValue]];
             }
             else {
                 id object = [NSObject objectOfClass:filteredArrayObj fromXML:newValue];
