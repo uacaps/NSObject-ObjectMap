f26f02777ab302a42d1b9c3fe375184a929d2b6c
Matthew York <myork@cs.ua.edu>
2013-11-13 15:28:15 -0600

    - Trying to fix xml testing

--- Tests/UnitTestsTests/XMLTestCase.m
+++ Tests/UnitTestsTests/XMLTestCase.m
@@ -39,12 +39,16 @@
     SingleObject *deserializedObject = [NSObject objectOfClass:@"SingleObject" fromXML:[testSingleObject XMLString]];
     
     //Test all properties
-    [TestHelpers testObject:testSingleObject withDeserializedVersion:deserializedObject forMethodNamed:@"testSingleObject" dataType:DataTypeXML];
+    [self testObject:testSingleObject withDeserializedVersion:deserializedObject forMethodNamed:@"testSingleObject" dataType:DataTypeXML];
 }
 
 -(void)testNestedObject{
     NestedObject *testObject = [[NestedObject alloc] init];
     testObject.singleObject = [SingleObject newSingleObject];
+    
+    NestedObject *deserializedObject = [NSObject objectOfClass:@"NestedObject" fromXML:[testObject XMLString]];
+    
+    [self testObject:testObject withDeserializedVersion:deserializedObject forMethodNamed:@"testNestedObject" dataType:DataTypeXML];
 }
 
 -(void)testTopLevelArray{
@@ -61,6 +65,35 @@
 
 -(void)testMissingProperties{
     
+}
+
+-(void)testObject:(id)obj withDeserializedVersion:(id)deserializedObj forMethodNamed:(NSString *)methodName dataType:(DataType)type {
+    //Test all properties
+    for (NSString *propertyName in [deserializedObj propertyDictionary]) {
+        if ([[deserializedObj valueForKey:propertyName] isKindOfClass:[NSDate class]]) {
+            XCTAssertEqualWithAccuracy([[obj valueForKey:propertyName] timeIntervalSince1970], [[deserializedObj valueForKey:propertyName] timeIntervalSince1970], 0.01, @"Failed %@ serialization/deserialization test for method named: %@. Failed on property %@", [self stringForType:type], methodName, propertyName);
+        }
+        else {
+            XCTAssertEqualObjects([obj valueForKey:propertyName], [deserializedObj valueForKey:propertyName], @"Failed %@ serialization/deserialization test for method named: %@. Failed on property %@", [self stringForType:type], methodName, propertyName);
+        }
+    }
+}
+
+- (NSString *)stringForType:(DataType)type {
+    switch (type) {
+        case DataTypeXML:
+            return @"XML";
+            break;
+        case DataTypeJSON:
+            return @"JSON";
+            break;
+        case DataTypeSOAP:
+            return @"SOAP";
+            break;
+        default:
+            return @"";
+            break;
+    }
 }
 
 @end
