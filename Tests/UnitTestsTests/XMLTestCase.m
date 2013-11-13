cf6e008708945be41aee5dc8210ee03a8fc70661
Matthew York <myork@cs.ua.edu>
2013-11-13 15:28:54 -0600

    - Connected xmlTestCase with parent class

--- Tests/UnitTestsTests/XMLTestCase.m
+++ Tests/UnitTestsTests/XMLTestCase.m
@@ -11,9 +11,9 @@
 #import "SingleObject.h"
 #import "NestedObject.h"
 #import "ObjectWithArray.h"
-#import "TestHelpers.h"
+#import "ObjectMapTestCase.m"
 
-@interface XMLTestCase : XCTestCase
+@interface XMLTestCase : ObjectMapTestCase
 
 @end
 
@@ -67,33 +67,5 @@
     
 }
 
--(void)testObject:(id)obj withDeserializedVersion:(id)deserializedObj forMethodNamed:(NSString *)methodName dataType:(DataType)type {
-    //Test all properties
-    for (NSString *propertyName in [deserializedObj propertyDictionary]) {
-        if ([[deserializedObj valueForKey:propertyName] isKindOfClass:[NSDate class]]) {
-            XCTAssertEqualWithAccuracy([[obj valueForKey:propertyName] timeIntervalSince1970], [[deserializedObj valueForKey:propertyName] timeIntervalSince1970], 0.01, @"Failed %@ serialization/deserialization test for method named: %@. Failed on property %@", [self stringForType:type], methodName, propertyName);
-        }
-        else {
-            XCTAssertEqualObjects([obj valueForKey:propertyName], [deserializedObj valueForKey:propertyName], @"Failed %@ serialization/deserialization test for method named: %@. Failed on property %@", [self stringForType:type], methodName, propertyName);
-        }
-    }
-}
-
-- (NSString *)stringForType:(DataType)type {
-    switch (type) {
-        case DataTypeXML:
-            return @"XML";
-            break;
-        case DataTypeJSON:
-            return @"JSON";
-            break;
-        case DataTypeSOAP:
-            return @"SOAP";
-            break;
-        default:
-            return @"";
-            break;
-    }
-}
 
 @end
