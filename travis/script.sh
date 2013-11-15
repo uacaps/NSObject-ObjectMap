03046f77d04ed5f97d23fefc6ee2ace43cefe07c
Ben Gordon <brgordon@ua.edu>
2013-11-15 10:21:46 -0600

    Update architectures again.

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool ONLY_ACTIVE_ARCH=NO -project Tests/UnitTests.xcodeproj -scheme UnitTests -sdk iphonesimulator test
+xctool VALID_ARCHS=i386 CURRENT_ARCH=i386 ONLY_ACTIVE_ARCH=YES -project Tests/UnitTests.xcodeproj -scheme UnitTests -sdk iphonesimulator test
