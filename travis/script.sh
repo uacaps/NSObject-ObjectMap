ad1455677e1d172e80957439d6ff8a93224e7865
Ben Gordon <brgordon@ua.edu>
2013-11-15 10:52:24 -0600

    And now we attempt to load in a new xctool

--- travis/script.sh
+++ travis/script.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 set -e
 
-xctool ARCHS=i386 ONLY_ACTIVE_ARCH=NO -project Tests/UnitTests.xcodeproj -scheme UnitTests -sdk iphonesimulator7.0 test
+xctool ARCHS=i386 ONLY_ACTIVE_ARCH=NO -project Tests/UnitTests.xcodeproj -scheme UnitTests -sdk iphonesimulator test
