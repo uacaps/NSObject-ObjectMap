f355ffd0a5adc4757384d07965095da6c29857c5
Ben Gordon <brgordon@ua.edu>
2013-11-15 10:29:43 -0600

    Removed storyboards for Travis-CI to work

--- Tests/UnitTests/AppDelegate.m
+++ Tests/UnitTests/AppDelegate.m
@@ -7,12 +7,16 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[ViewController alloc] initWithNibName:@"ViewController" bundle:nil];
+    [self.window makeKeyAndVisible];
     return YES;
 }
 							
