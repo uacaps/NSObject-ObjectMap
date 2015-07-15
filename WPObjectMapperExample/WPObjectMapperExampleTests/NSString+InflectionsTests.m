33230b594822681334582f7ea65d492c08e3fedc
TylerOrcutt <t.orcutt@eagle.clarion.edu>
2015-07-15 15:53:54 -0400

    IPADRAIN-1798 NSString+InflectionsTests

--- WPObjectMapperExample/WPObjectMapperExampleTests/NSString+InflectionsTests.m
+++ WPObjectMapperExample/WPObjectMapperExampleTests/NSString+InflectionsTests.m
@@ -6,4 +6,29 @@
 //  Copyright (c) 2015 The Washington Post. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <XCTest/XCTest.h>
+#import "NSString+Inflections.h"
+
+@interface NSStringInflectionsTests : XCTestCase
+@end
+
+@implementation NSStringInflectionsTests
+
+- (void) testUnderScore{
+    NSString * str = @"theQuickBrownFoxJumpedOverTheLazyDog";
+    NSString *underScore = [str underscore];
+    XCTAssertEqualObjects(underScore,@"the_quick_brown_fox_jumped_over_the_lazy_dog");
+}
+
+- (void) testCamelCase {
+    NSString *str =@"the_quick_brown_fox_jumped_over_the_lazy_dog";
+    NSString *camelCase = [str camelcase];
+    XCTAssertEqualObjects(camelCase, @"theQuickBrownFoxJumpedOverTheLazyDog");
+}
+
+- (void) testClasify {
+    NSString *str =@"the_quick_brown_fox_jumped_over_the_lazy_dog";
+    NSString *clasify = [str classify];
+    XCTAssertEqualObjects(clasify, @"TheQuickBrownFoxJumpedOverTheLazyDog");
+}
+@end
