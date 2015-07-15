68cf570e975e913b14c7d37bd8fa75d7cf66f380
TylerOrcutt <t.orcutt@eagle.clarion.edu>
2015-07-15 15:51:44 -0400

    IPADRAIN-1798 NSString+InflectionsTests

---
+++ WPObjectMapperExample/WPObjectMapperExampleTests/NSString+InflectionsTests.m
@@ -0,0 +1,9 @@
+//
+//  NSString+InflectionsTests.m
+//  WPObjectMapperExample
+//
+//  Created by Orcutt, Tyler on 7/15/15.
+//  Copyright (c) 2015 The Washington Post. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
